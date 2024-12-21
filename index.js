e346e02f0a1167229f6f502fee21e11ec26afde8
Aya <ayadaaboul02@outlook.com>
2024-12-21 08:56:20 +0200

    Completed Step 4

--- index.js
+++ index.js
@@ -27,6 +27,37 @@ app.get('/time', (req, res) => {
     });
 });
 
+app.get('/hello/ID', (req, res) => {
+    const id = req.params.ID;
+    if(id){
+        res.json({
+            status: 200,
+            message: 'Hello, '+id
+        });
+    }else{
+        res.json({
+            status: 200,
+            message: 'Hello'
+        });
+    }
+});
+
+app.get('/search', (req, res) => {
+    const search = req.query.s;
+    if (search) { // If 's' query is provided
+        res.status(200).json({
+            status: 200,
+            message: 'OK',
+            data: search
+        });
+    } else { // If 's' query is not provided
+        res.status(500).json({
+            status: 500,
+            error: true,
+            message: 'You have to provide a search'
+        });
+    }
+});
 app.listen(port, () => {
     console.log(`Server is listening on port`+port);
 });
