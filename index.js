a30b6fdfb7cc51caa91e55940246916d38948613
Aya <ayadaaboul02@outlook.com>
2024-12-21 08:15:35 +0200

    Step 3

--- index.js
+++ index.js
@@ -3,12 +3,19 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('*', (req, res) => {
-    console.log('Request received',req.url);
-    res.send('ok');
-});
+// app.get('*', (req, res) => {
+//     console.log('Request received',req.url);
+//     res.send('ok');
+// });
 
 app.get('/test', (req, res) => {
+    res.json({
+        status: 200,
+        message: 'ok'
+    });
+});
+
+app.get('/time', (req, res) => {
     const currentTime = new Date();
     const hours = currentTime.getHours();
     const minutes = currentTime.getMinutes();
