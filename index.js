2ed75a3219bff3354fc3daa5b98a4438efc6e6a7
Aya <ayadaaboul02@outlook.com>
2024-12-23 09:56:12 +0200

    step 5

--- index.js
+++ index.js
@@ -3,16 +3,27 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-// app.get('*', (req, res) => {
-//     console.log('Request received',req.url);
-//     res.send('ok');
-// });
+const movies = [
+    { title: 'Jaws', year: 1975, rating: 8 },
+    { title: 'Avatar', year: 2009, rating: 7.8 },
+    { title: 'Brazil', year: 1985, rating: 8 },
+    { title: 'الإرهاب والكباب‎', year: 1992, rating: 6.2 }
+]
+// CRUD create read update delete -> POST GET PUT DELETE
+//testing get can be through the browser (localhost:3000) (nodemon is an alternative)
+
+app.get('/', (req, res) => { // the slash / vs star * 
+// after slash no characters  after star any characters
+    console.log('Request received',req.url);
+    res.send('ok');
+});
 
 app.get('/test', (req, res) => {
     res.json({
         status: 200,
         message: 'ok'
     });
+    //res.json return the object but res.send returns the string
 });
 
 app.get('/time', (req, res) => {
@@ -50,7 +61,7 @@ app.get('/search', (req, res) => {
             message: 'OK',
             data: search
         });
-    } else { // If 's' query is not provided
+    } else { 
         res.status(500).json({
             status: 500,
             error: true,
@@ -58,6 +69,34 @@ app.get('/search', (req, res) => {
         });
     }
 });
+
+app.post('/movies/create', (req, res) => {
+    const movie=req.body;
+    movies.push(movie);
+    res.json({
+        status: 200,
+        message: 'Movie has been added',
+        data: movie
+    });
+
+
+});
+
+app.get('/movies/read', (req, res) => {
+    res.json({
+        status: 200,
+        data: movies
+    });
+
+});
+
+app.put('/movies/update', (req, res) => {
+    
+});
+
+app.delete('/movies/delete', (req, res) => {
+});
+
 app.listen(port, () => {
     console.log(`Server is listening on port`+port);
 });
